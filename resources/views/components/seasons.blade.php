c820d1fb9700bf78772bed3a6ac367fff39f141a
Moshkov123 <my.akaynt2021.2000@gmail.com>
2024-01-03 12:46:47 +0400

    верстка2

--- resources/views/components/seasons.blade.php
+++ resources/views/components/seasons.blade.php
@@ -22,3 +22,22 @@
         @endforeach
     </div>
 </div>
+
+
+<script>
+    function toggleEpisodes(season) {
+        var episodes = document.getElementById('episodes_' + season);
+        var arrow = document.querySelector('.season-header[onclick="toggleEpisodes(\'' + season + '\')"] .arrow');
+        if (episodes.style.display === "none") {
+            episodes.style.display = "block";
+            arrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" fill="currentColor" class="h-5 w-5">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                </svg>`; // Change the arrow to the second SVG
+        } else {
+            episodes.style.display = "none";
+            arrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" fill="currentColor" class="h-5 w-5" style="transform: scaleY(-1);">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                </svg>`; // Change the arrow to the first SVG
+        }
+    }
+</script>
